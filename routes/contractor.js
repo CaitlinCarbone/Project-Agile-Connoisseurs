eb1734b140269a5163957dced59514f260086ecd
CaitlinCarbone <73457571+CaitlinCarbone@users.noreply.github.com>
2023-03-03 22:29:20 -0500

    Bare Bones
    Added the basic html and javascript of the files

---
+++ routes/contractor.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const path = require('path')
+
+
+router.get("*", (req, res) => {
+    res.sendFile(path.resolve("views/contractorhomepage.html"));
+});
+
+
+module.exports = router;
