fe9fa12719784a5d7744051b16ec8845cd08cd18
Shindi <vendingmachinesfullofsnacks@outlook.com>
2023-03-06 18:10:14 -0500

    Added required files

--- data/sales.js
+++
@@ -1,7 +0,0 @@
-// add sign up and log out stuff for this type of user. add infromation to mongodb database (the config files are the database)
-
-//sign up 
-
-//delete user
-
-//change profile information
---
+++ server/tasks/seed.js
@@ -0,0 +1,1 @@
+// seed the database
