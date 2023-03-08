67336db9c62f2b217e4022c77d12d51c7216a53c
Lauren Ingraham <laureningy@gmail.com>
2023-03-07 22:21:45 -0500

    Update profile.js

--- routes/profile.js
+++ routes/profile.js
@@ -2,8 +2,8 @@ const express = require("express");
 const router = express.Router();
 const path = require("path");
 
-router.get("/", async (req, res) => {
-  // res.render
+router.get("*", async (req, res) => {
+  res.sendFile(path.resolve("static/profile.html"));
 });
 
 module.exports = router;
