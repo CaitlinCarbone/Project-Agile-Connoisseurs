8412a13ff59018113df3aad6613c4697a11ba196
Caitlin <caitlintcarbone@gmail.com>
2023-03-28 01:06:30 -0400

    Updated headers, made contact us, made log out, availabilitychecker

--- routes/index.js
+++ routes/index.js
@@ -14,6 +14,18 @@ const constructorMethod = (app) => {
   app.get("/home", (req, res) => {
     res.sendFile(path.resolve("static/home.html"));
   });
+  app.get("/availability", (req, res) => {
+    res.sendFile(path.resolve("static/availability.html"));
+  });
+  app.get("/newProject", (req, res) => {
+    res.sendFile(path.resolve("static/newproject.html"));
+  });
+  app.get("/salesinvoice", (req, res) => {
+    res.sendFile(path.resolve("static/salesinvoice.html"));
+  });
+  app.get("/error", (req, res) => {
+    res.sendFile(path.resolve("static/errorPage.html"));
+  });
   app.get("/profile", (req, res) => {
     res.sendFile(path.resolve("static/profile.html"));
   });
@@ -23,11 +35,23 @@ const constructorMethod = (app) => {
   app.use("/login", (req, res) => {
     res.sendFile(path.resolve("static/login.html"));
   });
+  app.use("/contact", (req, res) => {
+    res.sendFile(path.resolve("static/contactCustomerService.html"));
+  });
+  app.use("/customerBilling", (req, res) => {
+    res.sendFile(path.resolve("static/customerBilling.html"));
+  });
+  app.use("/contactHome", (req, res) => {
+    res.sendFile(path.resolve("static/contactHome.html"));
+  });
+  app.use("/logout", (req, res) => {
+    res.sendFile(path.resolve("static/home.html"));
+  });
   app.use("/register", (req, res) => {
     res.sendFile(path.resolve("static/register.html"));
   });
   app.use("*", (req, res) => {
-    res.redirect("/home");
+    res.redirect("/error");
   });
 };
 
