ca453d7abc5e3c156c3155005a064e4bbfaecdd0
Caitlin <caitlintcarbone@gmail.com>
2023-04-16 23:43:11 -0400

    Updated Sales Profile Page

--- routes/index.js
+++ routes/index.js
@@ -34,6 +34,9 @@ const constructorMethod = (app) => {
   app.get("/profile", (req, res) => {
     res.sendFile(path.resolve("static/profile.html"));
   });
+  app.get("/salesProfile", (req, res) => {
+    res.sendFile(path.resolve("static/sales-profile.html"));
+  });
   app.get("/chooseuserpage", (req, res) => {
     res.sendFile(path.resolve("static/chooseuserpage.html"));
   });
