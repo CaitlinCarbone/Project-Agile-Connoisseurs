bd52c4a11b81e34dc9daf0373b66efb07b89163c
Peter Rauscher <peterrauscher@protonmail.com>
2023-04-25 20:55:45 -0400

    Sales can create customers

--- routes/sales.js
+++ routes/sales.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const router = express.Router();
 const path = require("path");
+const { customerData } = require("../data");
 
-router.get("*", (req, res) => {
+router.get("/", async (req, res) => {
   res.sendFile(path.resolve("static/salespersonhomepage.html"));
+});
+
+router.get("/customers", async (req, res) => {
+  const allCustomers = await customerData.getAllCustomers();
+  return res.render("pages/sales/customers", {
+    title: "Customers",
+    css: "/css/manager.css",
+    layout: "sales",
+    customers: allCustomers,
+  });
 });
 
 module.exports = router;
