571922d2b56f53682ba05a676629e7e5c584ac9f
Peter Rauscher <peterrauscher@protonmail.com>
2023-04-26 11:00:00 -0400

    Fix sales->manager redirect on customer creation, improve error styling on login/register pages

--- routes/sales.js
+++ routes/sales.js
@@ -20,4 +20,38 @@ router.get("/customers", async (req, res) => {
   });
 });
 
+router.post("/customers", async (req, res) => {
+  let { email, password, firstName, lastName, address, city, state } = req.body;
+  try {
+    const result = await customerData.createCustomer(
+      email,
+      password,
+      firstName,
+      lastName,
+      address,
+      city,
+      state
+    );
+    if (result.success) {
+      const allCustomers = await customerData.getAllCustomers();
+      return res.render("pages/sales/customers", {
+        title: "Manage Customers",
+        layout: "manager",
+        css: "/css/manager.css",
+        error: null,
+        customers: allCustomers,
+      });
+    } else throw `Failed to create user`;
+  } catch (e) {
+    const allCustomers = await customerData.getAllCustomers();
+    res.render("pages/sales/customers", {
+      title: "Manage Customers",
+      layout: "manager",
+      error: e,
+      css: "/css/manager.css",
+      customers: allCustomers,
+    });
+  }
+});
+
 module.exports = router;
