6b7b87e61083372e194b3f31e36ddb51cfe16c67
Peter Rauscher <peterrauscher@protonmail.com>
2023-04-25 21:39:50 -0400

    Starting seed task

--- tasks/seed.js
+++ tasks/seed.js
@@ -1,1 +1,31 @@
-// seed the database
+const { userData, customerData, projectData } = require("../data");
+
+(async () => {
+  customerData.createCustomer(
+    "jane@gmail.com",
+    "password",
+    "Jane",
+    "Doe",
+    "123 Easy Street",
+    "Los Angeles",
+    "CA"
+  );
+  customerData.createCustomer(
+    "kyle@gmail.com",
+    "password",
+    "Kyle Henderson",
+    "Doe",
+    "456 Wallaby Way",
+    "Seattle",
+    "WA"
+  );
+  customerData.createCustomer(
+    "walter@gmail.com",
+    "password",
+    "Walter",
+    "White",
+    "308 Negra Arroyo Lane",
+    "Albuquerque",
+    "NM"
+  );
+})();
